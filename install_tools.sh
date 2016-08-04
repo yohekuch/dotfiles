0882b648d536390e3bc3be254443847b0b0baf82
Yohei Kuchiki <extentlambda@gmail.com>
2016-08-04 13:07:18 +0900

    build-essential, pae

--- install_tools.sh
+++ install_tools.sh
@@ -5,9 +5,8 @@ yes | sudo apt-get update
 yes | sudo apt-get upgrade
 
 # install tools
-yes | sudo apt-get install gcc
-yes | sudo apt-get install make
-yes | sudo apt-get install linux-headers-generic
+yes | sudo apt-get install build-essential
+yes | sudo apt-get install linux-headers-generic-pae
 yes | sudo apt-get install gnome-terminal
 yes | sudo apt-get install vim
 yes | sudo apt-get install ratpoison
