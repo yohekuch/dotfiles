1d382a571fe33c2ed29e3a547cbda02523ddc9ce
yohei <extentlambda@gmail.com>
2017-08-08 18:04:15 +0900

    Changed wget option and argument

--- init/init_font.sh
+++ init/init_font.sh
@@ -6,14 +6,14 @@ set -eu
 # http://jorrel.googlepages.com/Monaco_Linux.ttf
 # http://www.gringod.com/wp-upload/software/Fonts/Monaco_Linux.ttf
 
-FONTSDIR=${HOME}/Downloads/fonts
+FONTSDIR="${HOME}/Downloads/fonts"
 mkdir -p ${FONTSDIR}
 
 # Monaco_Linux
 MONACO_URL="http://jorrel.googlepages.com/Monaco_Linux.ttf"
 MONACO_FONT="Monaco_Linux.ttf"
 fc-list | grep ${MONACO_FONT} || \
-    wget -P ${FONTSDIR} -O ${MONACO_FONT} ${MONACO_URL}
+    wget -P "${FONTSDIR}" "${MONACO_URL}"
 
 # Install
 for font_file in ${FONTSDIR}/*.tt?; do
