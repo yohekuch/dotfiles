99feaab235f7dbb9189b05b77b76dba5dc4477fa
yohei <extentlambda@gmail.com>
2017-08-21 16:33:29 +0900

    Check if Dropbox directory exists. And add my .zshrc to prezto.

--- init/init_dropbox.sh
+++ init/init_dropbox.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 cd ${HOME}/Downloads
-wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-
+if [ ! -d ./.dropbox-dist ]; then
+    wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+fi
--- init/zsh/prezto.sh
+++ init/zsh/prezto.sh
@@ -8,8 +8,11 @@ if [ ! -d ${HOME}/.zprezto ]; then
 		"${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-# Create a new Zsh configuration by copying the Zsh configuration files provided
+# Append my zsh settings
 setopt EXTENDED_GLOB
+cat ${HOME}/dotfiles/init/zsh/.zshrc >>"${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshrc"
+
+# Create a new Zsh configuration by copying the Zsh configuration files provided
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
